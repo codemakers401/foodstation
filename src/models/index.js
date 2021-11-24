ed9d871f3885a36e32b24d9c65db257c4bc1b032
AMR NZZAL <amr.nzzal@gmail.com>
2021-11-25 01:37:42 +0200

    data base

--- src/models/index.js
+++ src/models/index.js
@@ -1,1 +1,59 @@
-// index model
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql://localhost:5432/foodstation";
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+
+let sequelize = new Sequelize(DATABASE_URL, seqOptions);
+
+const itemsSchema = require('./items-model');
+const orderStatusSchema = require('./order-status');
+const ordersSchema = require('./orders-model');
+const restaurantSchema = require('./restaurant-model');
+const userSchema = require('./users-model');
+
+
+const itemsModel = itemsSchema(sequelize, DataTypes);
+const orderStatusModel = orderStatusSchema(sequelize, DataTypes);
+const ordersModel = ordersSchema(sequelize, DataTypes);
+const restaurantModel = restaurantSchema(sequelize, DataTypes);
+const userModel = userSchema(sequelize, DataTypes);
+
+userModel.hasMany(ordersModel, { foreignKey: 'customerID', sourceKey: 'id' });
+ordersModel.belongsTo(userModel, { foreignKey: 'customerID', targetKey: 'id' })
+
+itemsModel.hasMany(ordersModel, { foreignKey: 'itemID', sourceKey: 'id' });
+ordersModel.belongsTo(itemsModel, { foreignKey: 'itemID', targetKey: 'id' })
+
+orderStatusModel.hasMany(ordersModel, { foreignKey: 'statusID', sourceKey: 'id' });
+ordersModel.belongsTo(orderStatusModel, { foreignKey: 'statusID', targetKey: 'id' })
+
+restaurantModel.hasMany(itemsModel, { foreignKey: 'restaID', sourceKey: 'id' });
+itemsModel.belongsTo(restaurantModel, { foreignKey: 'restaID', targetKey: 'id' })
+
+const Collection = require('./collection');
+
+const itemsCollection = new Collection(itemsModel);
+const orderStatusCollection = new Collection(orderStatusModel);
+const ordersCollection = new Collection(ordersModel);
+const restaurantCollection = new Collection(restaurantModel);
+const userCollection = new Collection(userModel);
+
+
+
+module.exports = {
+  db: sequelize,
+  itemsCollection: itemsCollection,
+  orderStatusCollection: orderStatusCollection,
+  ordersCollection: ordersCollection,
+  restaurantCollection: restaurantCollection,
+  userCollection: userCollection
+}
---
+++ src/models/users-model.js
@@ -0,0 +1,59 @@
+'use strict';
+
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET || 'zxcvbnm';
+
+const userSchema = (sequelize, DataTypes) => {
+  const model = sequelize.define('users', {
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    userRole: { type: DataTypes.ENUM('Admin', 'Customer'), defaultValue: 'Customer' },
+    userAddress: { type: DataTypes.STRING, allowNull: false },
+    userPhone: { type: DataTypes.STRING, allowNull: false },
+    token: { type: DataTypes.VIRTUAL },
+    actions: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        const acl = {
+          Customer: ['read'],
+          Admin: ['read', 'create', 'update', 'delete']
+        }
+        return acl[this.userRole];
+      }
+    }
+  });
+  // we attached a function to our Users Model
+  model.authenticateBasic = async function (username, password) {
+    // get the user form the database 
+    const user = await model.findOne({ where: { username } }); // select * from Users where username='tamim';
+    // compare the users' password from the DB with the on that was submitted in the form
+    const valid = await bcrypt.compare(password, user.password);
+    // if the user is validated, we will create a new token for that user using the jsonwebtokenlibaray
+    if (valid) {
+      let newToken = jwt.sign({ username: user.username }, SECRET);
+      user.token = newToken;
+      return user;
+    } else {
+      throw new Error('Invalid User');
+    }
+  }
+
+  model.authenticateBearer = async function (token) {
+    // check with the jwt if the token is proper
+    const parsedToken = jwt.verify(token, SECRET); // the parsed token payload, we are parsing the data using the Secret Key
+    // then find a user that has the data from the payload
+    const user = await this.findOne({ where: { username: parsedToken.username } });////###################
+    // if there is, then get the user model
+    if (user) {
+      return user;
+    } else {
+      // if not, throw error
+      throw new Error('Invalid Token');
+    }
+  }
+
+
+}
+module.exports = userSchema;
