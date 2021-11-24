645aa7b517c5140d98ebfd3bdde8930ee38c83ac
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-11-24 16:02:48 +0200

    ok

--- src/server.js
+++ src/server.js
@@ -1,24 +1,30 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors')
+require('dotenv').config()
+
 
 const notFoundHandler = require('./error-handlers/404.js');
 const errorHandler = require('./error-handlers/500.js');
 // const logger = require('./middleware/logger.js');
 
-const authRouter = require('./routes/routes')
-const v1Routes = require('./routes/v1.js');
-const v2Routes = require('./routes/v2.js');
+// const authRouter = require('./routes/routes')
+// const v1Routes = require('./routes/v1.js');
+// const v2Routes = require('./routes/v2.js');
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
+app.use(express.urlencoded({
+  extended:true
+}))
 // app.use(logger);
 
-app.use(authRouter)
-app.use('/api/v1', v1Routes);
-app.use('/api/v2', v2Routes);
+// app.use(authRouter)
+// app.use('/api/v1', v1Routes);
+// app.use('/api/v2', v2Routes);
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
