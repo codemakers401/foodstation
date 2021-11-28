82df0a2ba0b94ed4e4203ee07e7dafa65084277f
MunesYasin <m2nesyasin@gmail.com>
2021-11-28 22:18:34 +0200

    update test file

--- __tests__/status.test.js
+++ __tests__/status.test.js
@@ -17,8 +17,8 @@ afterAll(async () => {
 
 
 let users = {
-    Admin: { username: 'wwwwq', password: 'password', userRole: 'Admin' ,userAddress :"Amman ",userPhone:"33222", userEmail :"wwewewewew"},
-    Customer: { username: 'ahmed', password: 'password', userRole: 'Customer' ,userAddress :"irbid ",userPhone:"545454", userEmail :"wwewew",},
+    Admin: { username: 'munes', password: 'password', userRole: 'Admin' ,userAddress :"Amman ",userPhone:"079", userEmail :"munes1"},
+    Customer: { username: 'ahmed', password: 'password', userRole: 'Customer' ,userAddress :"irbid ",userPhone:"078", userEmail :"ahmed1",},
    
 };
 
@@ -29,8 +29,8 @@ let status = {
     StatusName : "avalibale"
 }
 
-describe('Stutas', () => {
-    it('can create one', async () => {
+describe('check status route with admin', () => {
+    it('create admin account ', async () => {
         const response = await mockRequest.post('/signup').send(users.Admin);
         const userObject = response.body;
         expect(response.status).toBe(201);
@@ -45,7 +45,7 @@ describe('Stutas', () => {
 
     });
 
-    it('can sign-in with basic header', async () => {
+    it('can sign-in with basic header admin', async () => {
         const response = await mockRequest.post('/signin').auth(users.Admin.username, users.Admin.password);
         const userObject = response.body;
         expect(response.status).toBe(200);
@@ -55,7 +55,7 @@ describe('Stutas', () => {
         expect(userObject.user.userRole).toEqual(users.Admin.userRole);
     });
 
-   it('can sign-in with bearer headers', async () => {
+   it('can sign-in with bearer headers admin', async () => {
         const response = await mockRequest.post('/signin').auth(users.Admin.username, users.Admin.password);
         const token = response.body.token;
         const bearerResponse = await mockRequest.get('/users').set('Authorization', `Bearer ${token}`)
@@ -63,7 +63,7 @@ describe('Stutas', () => {
     });
         
         
-            it('create new status', async () => {
+            it('create new status by admin', async () => {
                 const auth = await mockRequest.post('/signin').auth(users.Admin.username, users.Admin.password);
 
                 const response = await mockRequest.post('/status').send(status).set('Authorization', `Bearer ${auth.body.token}`);
@@ -74,7 +74,7 @@ describe('Stutas', () => {
                 
             })
 
-            it('get spicific status', async () => {
+            it('get spicific status by admin', async () => {
                 const auth = await mockRequest.post('/signin').auth(users.Admin.username, users.Admin.password);
 
                 const response = await mockRequest.get('/status/1').set('Authorization', `Bearer ${auth.body.token}`);
@@ -85,7 +85,7 @@ describe('Stutas', () => {
                 
             })
         
-            it('Update a status', async () => {
+            it('Update a status by admin', async () => {
                 const auth = await mockRequest.post('/signin').auth(users.Admin.username, users.Admin.password);
                  let newStaus = {
                     StatusName : "not avalibale"
@@ -100,7 +100,7 @@ describe('Stutas', () => {
             })
         
 
-            it('Get all status', async () => {
+            it('Get all status by admin', async () => {
                  let newStaus = {
                     StatusName : "not avalibale"
                    }
@@ -117,7 +117,76 @@ describe('Stutas', () => {
                 expect(userObject[0].StatusName).toEqual(newStaus.StatusName);
 
                 
+            }) 
+        })
+            describe('check status route with customer', () => {
+
+            it('Create customer account', async () => {
+                const response = await mockRequest.post('/signup').send(users.Customer);
+                const userObject = response.body;
+                expect(response.status).toBe(201);
+                expect(userObject.user.id).toBeDefined();
+        
+                expect(userObject.user.username).toEqual(users.Customer.username);
+                expect(userObject.user.userRole).toEqual(users.Customer.userRole);
+                expect(userObject.user.userAddress).toEqual(users.Customer.userAddress);
+                expect(userObject.user.userPhone).toEqual(users.Customer.userPhone);
+                expect(userObject.user.userEmail).toEqual(users.Customer.userEmail);
+               
+        
+            });
+        
+            it('can sign-in with basic header customer', async () => {
+                const response = await mockRequest.post('/signin').auth(users.Customer.username, users.Customer.password);
+                const userObject = response.body;
+                expect(response.status).toBe(200);
+                expect(userObject.user.token).toBeDefined();
+                expect(userObject.user.id).toBeDefined();
+                expect(userObject.user.username).toEqual(users.Customer.username);
+                expect(userObject.user.userRole).toEqual(users.Customer.userRole);
+            });
+        
+       
+                
+
+            it('create new status by customer', async () => {
+                const auth = await mockRequest.post('/signin').auth(users.Customer.username, users.Customer.password);
+
+                const response = await mockRequest.post('/status').send(status).set('Authorization', `Bearer ${auth.body.token}`);
+                const userObject = response.body;
+                expect(response.status).toBe(500);
+                expect(userObject.message).toEqual("Access Denied  --- acl");
+                
             })
+
+            it('get spicific status by customer', async () => {
+                const auth = await mockRequest.post('/signin').auth(users.Customer.username, users.Customer.password);
+
+                const response = await mockRequest.get('/status/1').set('Authorization', `Bearer ${auth.body.token}`);
+                const userObject = response.body;
+                expect(response.status).toBe(500);
+                expect(userObject.message).toEqual("Access Denied  --- acl");
+                
+            })
+        
+            it('Update a status by customer', async () => {
+                const auth = await mockRequest.post('/signin').auth(users.Customer.username, users.Customer.password);
+                 let newStaus = {
+                    StatusName : "not avalibale"
+                   }
+                   
+                const response = await mockRequest.put('/status/1').send(newStaus).set('Authorization', `Bearer ${auth.body.token}`);
+                const userObject = response.body;
+                expect(response.status).toBe(500);
+                expect(userObject.message).toEqual("Access Denied  --- acl");
+                
+            })
+
+
+
+
+
+
 
 
         });
--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-
-const DATABASE_URL =  process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;;
+require('dotenv').config()
+const DATABASE_URL =  process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   dialectOptions: {
     ssl: {
